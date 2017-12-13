1914942ecc22811e2b62c926434c3ff08a3592ea
Wade Tandy <wtandy@bloomberg.net>
2017-12-13 00:59:10 -0500

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-export * from './main';
+import 'object-assign-shim';
+import * as es6Promise from 'es6-promise';
+es6Promise.polyfill();
+
+import patchExtends from './custom-extend';
+patchExtends();
+
+import Config from './configuration';
+import Model from './model';
+import Attribute from './attribute';
+import attrDecorator from './util/attr-decorator';
+import { hasMany, hasOne, belongsTo } from './associations';
+
+const attr = function(opts?: attributeOptions): any {
+  return new Attribute(opts);
+};
+
+export { Config, Model, attr, attrDecorator, hasMany, hasOne, belongsTo, patchExtends };
