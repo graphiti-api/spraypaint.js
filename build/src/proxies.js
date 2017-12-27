a26699e33c4bd41f701c5320ac60d810d9184520
Wade Tandy <wtandy@bloomberg.net>
2017-12-27 16:33:33 -0500

    Ignore build dir

--- build/src/proxies.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var collection_proxy_1 = require("./proxies/collection-proxy");
-exports.CollectionProxy = collection_proxy_1.default;
-var record_proxy_1 = require("./proxies/record-proxy");
-exports.RecordProxy = record_proxy_1.default;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHJveGllcy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9wcm94aWVzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsK0RBQXdEO0FBSXRELDBCQUpLLDBCQUFlLENBSUw7QUFIakIsdURBQWdEO0FBSTlDLHNCQUpLLHNCQUFXLENBSUwifQ==
--- build/src/util/snakecase.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var lodash_1 = require("lodash");
-exports.default = lodash_1.snakeCase;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic25ha2VjYXNlLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vLi4vc3JjL3V0aWwvc25ha2VjYXNlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsaUNBQThDO0FBQXJDLDJCQUFBLFNBQVMsQ0FBVyJ9
