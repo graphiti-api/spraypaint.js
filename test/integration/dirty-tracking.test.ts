1e170f57e62633272454db7593237d7233056cb8
Lee Richmond <richmolj@gmail.com>
2018-01-23 16:04:36 -0500

    Fix spec (#61)

--- test/integration/dirty-tracking.test.ts
+++ test/integration/dirty-tracking.test.ts
@@ -2,31 +2,31 @@ import { sinon, expect, fetchMock } from "../test-helper"
 import { Person, Author, Book } from "../fixtures"
 import { IResultProxy } from "../../src/proxies/index"
 
-afterEach(() => {
-  fetchMock.restore()
-})
-
-let responsePayload = (firstName: string) => {
-  return {
-    data: {
-      id: "1",
-      type: "people",
-      attributes: { firstName }
-    }
-  }
-}
-
-beforeEach(() => {
-  let url = "http://example.com/api/v1/authors"
-  fetchMock.post(url, responsePayload('John'))
-  fetchMock.put(`${url}/1`, responsePayload('Jake'))
-})
-
 // This is a Vue-specific test. Since isPersisted is already true,
 // Vue will prevent the setter from firing. We cannot rely on
 // side-effect behavior of model.isPersisted = true
 // So, ensure we at least call reset() explicitly
 describe("Dirty tracking", () => {
+  let responsePayload = (firstName: string) => {
+    return {
+      data: {
+        id: "1",
+        type: "people",
+        attributes: { firstName }
+      }
+    }
+  }
+
+  afterEach(() => {
+    fetchMock.restore()
+  })
+
+  beforeEach(() => {
+    let url = "http://example.com/api/v1/authors"
+    fetchMock.post(url, responsePayload('John'))
+    fetchMock.put(`${url}/1`, responsePayload('Jake'))
+  })
+
   describe("when persisted, dirty, updated", () => {
     it("calls reset()", async () => {
       let instance = new Author({ firstName: 'John' })
