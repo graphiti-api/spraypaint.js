1758fef417e853563b22c5c04eea5d5d1391210e
Wade Tandy <wtandy@bloomberg.net>
2019-01-28 12:30:00 -0500

    Allow Number types not to write empty string
    Previously there was an issue when binding a Spraypaint model to a form
    or otherwise allowing a number field to be set to an empty string.  This
    would pass down to the backend and Graphiti would (correctly) blow up,
    as it was receiving a string but expecting a number (or null, in this
    case).  This will obey specyfing `Attr({type: Number})` in the model
    definition.  When this receives an empty string during payload
    serialization, it will write `null` instead.
    
    This is a quick fix to a specific user problem.  There is a bigger
    commit that needs to happen here with casting & payload
    serialization/deserialization, but I'd like to get this in to fix the
    immediate concern for now.

--- test/unit/write-payload.test.ts
+++ test/unit/write-payload.test.ts
@@ -3,6 +3,23 @@ import { WritePayload } from "../../src/util/write-payload"
 import { Person, PersonWithDasherizedKeys } from "../fixtures"
 
 describe("WritePayload", () => {
+  it("Does not serialize number attributes as empty string", () => {
+    let person = new Person({ first_name: "Joe", age: 23 })
+    ;(person.age as any) = ""
+    person.lastName = ""
+    let payload = new WritePayload(person)
+    expect(payload.asJSON()).to.deep.equal({
+      data: {
+        type: "people",
+        attributes: {
+          first_name: "Joe",
+          last_name: "",
+          age: null
+        }
+      }
+    })
+  })
+
   it("underscores attributes", () => {
     let person = new Person({ first_name: "Joe" })
     let payload = new WritePayload(person)
