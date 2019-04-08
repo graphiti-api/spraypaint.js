e25d39a7e692381ce860e9f1758f97d3c7eb6524
Lee Richmond <lrichmond1@bloomberg.net>
2019-04-08 11:23:04 -0400

    Allow POST with _method PATCH
    In extremely rare cases, companies will not allow the PATCH HTTP verb.
    For a longer explanation of this issue, see https://github.com/equivalent/scrapbook2/blob/master/archive/blogs/2016-12-12-put-vs-patch-vs-your-firewal.md
    
    The solution is to send a POST alongside the `X-HTTP-Method-Override`
    header. When the server adds `Rack::MethodOverride`, the header will
    take precedence (making the server think it's a PATCH).
    
    You can enable this behavior by setting the `patchAsPost` flag:
    
    ```ts
    class ApplicationRecord extends SpraypaintBase {
      // ... code ...
      static patchAsPost = true
    }
    ```

--- test/integration/nested-persistence.test.ts
+++ test/integration/nested-persistence.test.ts
@@ -243,7 +243,7 @@ describe("nested persistence", () => {
     // todo test unique includes/circular relationshio
     it("sends the correct payload", async () => {
       await instance.save({ with: { books: "genre", specialBooks: {} } })
-      
+
       expect(payloads[0]).to.deep.equal(expectedCreatePayload)
     })
 
@@ -291,7 +291,9 @@ describe("nested persistence", () => {
 
       it("should not be sent in the payload", async () => {
         await instance.save({ with: { books: "genre" } })
-        expect((<any>payloads)[0].included.some((i: any) => i['types'] === 'genre')).to.be.false
+        expect(
+          (<any>payloads)[0].included.some((i: any) => i["types"] === "genre")
+        ).to.be.false
       })
     })
 
