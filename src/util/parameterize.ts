da0ce789b2455943071cd43d2802a36ad5a6b739
Lee Richmond <lrichmond1@bloomberg.net>
2019-06-04 10:34:39 -0400

    Encode param values when IE
    IE does not automatically encode, so let's do it for our users
    automatically. This also checks if the value is already encoded (naively
    checking for "%") and avoids if so.

--- src/util/parameterize.ts
+++ src/util/parameterize.ts
@@ -3,7 +3,7 @@ const parameterize = (obj: any, prefix?: string): string => {
 
   for (let key in obj) {
     if (obj.hasOwnProperty(key)) {
-      const value = obj[key]
+      let value = obj[key]
 
       if (value !== undefined && value !== null && value !== "") {
         if (prefix) {
@@ -11,13 +11,16 @@ const parameterize = (obj: any, prefix?: string): string => {
         }
 
         if (Array.isArray(value)) {
+          value = value.map(v => {
+            return maybeEncode(v)
+          })
           if (value.length > 0) {
             str.push(`${key}=${value.join(",")}`)
           }
         } else if (typeof value === "object") {
           str.push(parameterize(value, key))
         } else {
-          str.push(`${key}=${value}`)
+          str.push(`${key}=${maybeEncode(value)}`)
         }
       }
     }
@@ -29,6 +32,15 @@ const parameterize = (obj: any, prefix?: string): string => {
   })
 
   return str.join("&")
+}
+
+// IE does not encode by default like other browsers
+const maybeEncode = (value: string): string => {
+  const isBrowser = typeof window !== "undefined"
+  const isIE = isBrowser && window.navigator.userAgent.match(/(MSIE|Trident)/)
+  const isEncoded = typeof value === "string" && value.indexOf("%") !== -1
+  const shouldEncode = isBrowser && isIE && !isEncoded
+  return shouldEncode ? encodeURIComponent(value) : value
 }
 
 export { parameterize as default }
