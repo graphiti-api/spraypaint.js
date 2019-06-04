60df3a98535d61910d9e4141f2a9ea52165731b8
Yoann Maitre <yoann@hellopret.fr>
2019-06-04 19:20:49 +0200

    Merge all validation tests in a single file

--- test/integration/validations.test.ts
+++ test/integration/validations.test.ts
@@ -1,82 +1,10 @@
 import { expect, sinon, fetchMock } from "../test-helper"
-import { Author, Book, Genre } from "../fixtures"
+import { Author, Book, Genre, Person, PersonDetail } from "../fixtures"
 import { tempId } from "../../src/util/temp-id"
 import { SpraypaintBase, ModelRecord } from "../../src/model"
 import { ValidationError } from "../../src/validation-errors"
 
-const mockErrors = {
-  firstName: {
-    code: "unprocessable_entity",
-    status: "422",
-    title: "Validation Error",
-    detail: "First Name cannot be blank",
-    meta: { attribute: "first_name", message: "cannot be blank" }
-  },
-  lastName: {
-    code: "unprocessable_entity",
-    status: "422",
-    title: "Validation Error",
-    detail: "Last Name cannot be blank",
-    meta: { attribute: "last-name", message: "cannot be blank" }
-  },
-  bookTitle: {
-    code: "unprocessable_entity",
-    status: "422",
-    title: "Validation Error",
-    detail: "Title cannot be blank",
-    meta: {
-      relationship: {
-        name: "books",
-        type: "books",
-        ["temp-id"]: "abc1",
-        attribute: "title",
-        message: "cannot be blank"
-      }
-    }
-  },
-  bookGenreName: {
-    code: "unprocessable_entity",
-    status: "422",
-    title: "Validation Error",
-    detail: "Name cannot be blank",
-    meta: {
-      relationship: {
-        name: "books",
-        type: "books",
-        ["temp-id"]: "abc1",
-        relationship: {
-          name: "genre",
-          type: "genres",
-          id: "1",
-          attribute: "name",
-          message: "cannot be blank"
-        }
-      }
-    }
-  },
-  bookGenreBase: {
-    code: "unprocessable_entity",
-    status: "422",
-    title: "Validation Error",
-    detail: "base some error",
-    meta: {
-      relationship: {
-        name: "books",
-        type: "books",
-        ["temp-id"]: "abc1",
-        relationship: {
-          name: "genre",
-          type: "genres",
-          id: "1",
-          attribute: "base",
-          message: "some error"
-        }
-      }
-    }
-  }
-} as any
-
-const resetMocks = () => {
+const resetMocks = (mockErrors: any) => {
   fetchMock.restore()
 
   let errors = []
@@ -96,11 +24,84 @@ const resetMocks = () => {
   })
 }
 
-let instance: Author
 let tempIdIndex = 0
-describe("validations", () => {
+describe("validations (1/2)", () => {
+  const mockErrors = {
+    firstName: {
+      code: "unprocessable_entity",
+      status: "422",
+      title: "Validation Error",
+      detail: "First Name cannot be blank",
+      meta: { attribute: "first_name", message: "cannot be blank" }
+    },
+    lastName: {
+      code: "unprocessable_entity",
+      status: "422",
+      title: "Validation Error",
+      detail: "Last Name cannot be blank",
+      meta: { attribute: "last-name", message: "cannot be blank" }
+    },
+    bookTitle: {
+      code: "unprocessable_entity",
+      status: "422",
+      title: "Validation Error",
+      detail: "Title cannot be blank",
+      meta: {
+        relationship: {
+          name: "books",
+          type: "books",
+          ["temp-id"]: "abc1",
+          attribute: "title",
+          message: "cannot be blank"
+        }
+      }
+    },
+    bookGenreName: {
+      code: "unprocessable_entity",
+      status: "422",
+      title: "Validation Error",
+      detail: "Name cannot be blank",
+      meta: {
+        relationship: {
+          name: "books",
+          type: "books",
+          ["temp-id"]: "abc1",
+          relationship: {
+            name: "genre",
+            type: "genres",
+            id: "1",
+            attribute: "name",
+            message: "cannot be blank"
+          }
+        }
+      }
+    },
+    bookGenreBase: {
+      code: "unprocessable_entity",
+      status: "422",
+      title: "Validation Error",
+      detail: "base some error",
+      meta: {
+        relationship: {
+          name: "books",
+          type: "books",
+          ["temp-id"]: "abc1",
+          relationship: {
+            name: "genre",
+            type: "genres",
+            id: "1",
+            attribute: "base",
+            message: "some error"
+          }
+        }
+      }
+    }
+  } as any
+
+  let instance: Author
+
   beforeEach(() => {
-    resetMocks()
+    resetMocks(mockErrors)
   })
 
   beforeEach(() => {
@@ -108,7 +109,6 @@ describe("validations", () => {
       tempIdIndex++
       return `abc${tempIdIndex}`
     })
-
     instance = new Author({ lastName: "King" })
     const genre = new Genre({ id: "1" })
     genre.isPersisted = true
@@ -275,6 +275,67 @@ describe("validations", () => {
         fullMessage: "some error",
         message: "some error",
         rawPayload: mockErrors.bookGenreBase
+      }
+    })
+  })
+})
+
+describe("validations (2/2)", () => {
+  const mockErrors = {
+    personDetailBase: {
+      code: "unprocessable_entity",
+      status: "422",
+      title: "Validation Error",
+      detail: "base some error",
+      meta: {
+        relationship: {
+          name: "person_detail",
+          type: "person_details",
+          ["temp-id"]: "abc1",
+          id: "1",
+          attribute: "base",
+          message: "some error"
+        }
+      }
+    }
+  } as any
+
+  let instance: Person
+
+  beforeEach(() => {
+    resetMocks(mockErrors)
+  })
+
+  beforeEach(() => {
+    sinon.stub(tempId, "generate").callsFake(() => {
+      tempIdIndex++
+      return `abc${tempIdIndex}`
+    })
+
+    const personDetail = new PersonDetail({ id: "1" })
+    personDetail.isPersisted = true
+    instance = new Person({
+      personDetail
+    })
+  })
+
+  afterEach(() => {
+    tempIdIndex = 0
+    ;(<any>tempId.generate).restore()
+  })
+
+  it("applies errors to nested belongsTo relationships with underscores", async () => {
+    const isSuccess = await instance.save({ with: ["person_details"] })
+    expect(instance.isPersisted).to.eq(false)
+    expect(isSuccess).to.eq(false)
+    expect(instance.personDetail.errors).to.deep.equal({
+      base: {
+        title: "Validation Error",
+        attribute: "base",
+        code: "unprocessable_entity",
+        fullMessage: "some error",
+        message: "some error",
+        rawPayload: mockErrors.personDetailBase
       }
     })
   })
