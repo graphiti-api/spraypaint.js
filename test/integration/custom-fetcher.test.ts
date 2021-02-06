9393050a9de212f7b15804dc1e8a46b99eabbb3d
Jake Correa <jcorrea257@gmail.com>
2021-02-05 19:02:37 -0800

    add more custom fetcher tests

--- test/integration/custom-fetcher.test.ts
+++ test/integration/custom-fetcher.test.ts
@@ -1,13 +1,17 @@
 import { expect, sinon } from "../test-helper"
-import { Person } from "../fixtures"
 import fetchMock = require("fetch-mock")
+import { Model, SpraypaintBase } from "../../src"
 
 describe("Custom fetcher", () => {
-  const oldFetch = Person.fetcher
-  let spy: sinon.SinonSpy
+  let spy1: sinon.SinonSpy
+  let spy2: sinon.SinonSpy
+
+  let ApplicationRecord: typeof SpraypaintBase
+  let Author: typeof SpraypaintBase
+  let Person: typeof SpraypaintBase
 
   beforeEach(() => {
-    spy = sinon.spy(() =>
+    const fetcher = (a: any, b: any) =>
       Promise.resolve(
         new Response(
           JSON.stringify({
@@ -21,20 +25,46 @@ describe("Custom fetcher", () => {
           })
         )
       )
-    )
 
-    Person.fetcher = spy
+    spy1 = sinon.spy(fetcher)
+    spy2 = sinon.spy(fetcher)
+
+    @Model({
+      baseUrl: "http://example.com",
+      apiNamespace: "/api/v1"
+    })
+    class Base extends SpraypaintBase {}
+    ApplicationRecord = Base
+
+    @Model({
+      jsonapiType: "authors",
+      fetcher: spy1
+    })
+    class A extends ApplicationRecord {}
+    Author = A
+
+    @Model({ jsonapiType: "people" })
+    class B extends ApplicationRecord {
+      static fetcher = spy2
+    }
+    Person = B
   })
 
-  afterEach(() => {
-    Person.fetcher = oldFetch
-  })
+  describe("uses custom fetchers", () => {
+    it("calls a custom fetcher set via config", async () => {
+      await Author.find(1)
 
-  it("calls the custom fetcher", async () => {
-    await Person.find(1)
+      expect(spy1).to.have.been.calledOnce.and.to.have.been.calledWith(
+        "http://example.com/api/v1/authors/1"
+      )
+    })
 
-    expect(spy).to.have.been.calledOnce.and.to.have.been.calledWith(
-      "http://example.com/api/v1/people/1"
-    )
+    it("calls a custom fetcher set via static props", async () => {
+      await Person.find(1)
+
+      expect(spy2).to.have.been.calledOnce.and.to.have.been.calledWith(
+        "http://example.com/api/v1/people/1"
+      )
+    })
   })
 })
